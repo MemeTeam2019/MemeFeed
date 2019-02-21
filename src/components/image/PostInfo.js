e651091dd6313c029e8f207fef72afba5f76d8fd
Emma Pedersen <erpeders.ucsc.edu>
2019-02-21 11:01:49 -0800

    pulling react counts

--- src/components/image/PostInfo.js
+++ src/components/image/PostInfo.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import {Button, StyleSheet, View, Image, Text } from 'react-native';
+import firebase from "react-native-firebase";
 
 class PostInfo extends React.Component{
+
+constructor(){
+  super();
+  this.state = {
+    reactCount: 0,
+    source: "author"
+  };
+}
+
+componentDidMount() {
+    this.getNumReaction();
+}
+
+getNumReaction(){
+  const memeid = this.props.memeId
+  const ref = firebase.firestore().collection("Memes").doc(memeid)
+
+  ref.get().then(docSnapshot => {
+      const data = docSnapshot.data();
+      console.log(data);
+      const reactCount = data.reactCount || 0;
+      this.setState({reactCount: reactCount})
+      //this.setState({source: author})
+  })
+}
+
 render() {
-return (
+  return (
     <View style={styles.postInfo}>
-        <Text style={{fontStyle: 'italic', fontWeight: 'bold', marginLeft: '2.5%'}}>source</Text>
-        <Text style={{fontWeight: 'bold', paddingTop: 3, marginLeft: '2.5%'}}>20 Reactions</Text>
-        <Text style={{fontWeight: 'bold', paddingTop: 10, marginLeft: '2.5%'}}>username
-            <Text style={{fontWeight: 'normal'}}> comment</Text>
-        </Text>
+      <Text style={{fontStyle: 'italic', fontWeight: 'bold', marginLeft: '2.5%'}}>{this.state.source}</Text>
+      <Text style={{fontWeight: 'bold', paddingTop: 3, marginLeft: '2.5%'}}>{this.state.reactCount} Reactions</Text>
+      <Text style={{fontWeight: 'bold', paddingTop: 10, marginLeft: '2.5%'}}>username
+          <Text style={{fontWeight: 'normal'}}> comment</Text>
+      </Text>
     </View>
     );
-}
+  }
 }
 
 export default PostInfo;
 
 const styles = StyleSheet.create({
-    container: {
-        fontSize: 16,
-        fontFamily: 'AvenirNext-Regular',
-        width: '100%',
-        height: 100,
-    },
-  });
+  container: {
+      fontSize: 16,
+      fontFamily: 'AvenirNext-Regular',
+      width: '100%',
+      height: 100,
+  },
+});
