6efec41b5fb71de2c0e7eff404b03eb2f4bb7569
Siddhi Panchal <siddhipanchal@Siddhis-MacBook-Pro.local>
2019-04-19 17:51:25 -0700

    info page on profile

---
+++ src/screens/aboutInfo3.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { StyleSheet, View, Button, ImageBackground, Text } from 'react-native';
+
+export default class LoginScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <ImageBackground
+        source={require('../images/white.png')}
+        style={styles.background}
+      >
+        <View style={styles.container}>
+          <Text style={styles.title}>Coming Soon</Text>
+        </View>
+        <View style={styles.aboutText}>
+          <Text style={styles.aboutText}>
+          Meme feed is currently in Beta, so be sure to update the application
+          to get features such as Meme upload, curated feeds, and DMing.
+          </Text>
+        </View>
+        <View style={styles.nextBut}>
+          <Button
+            title="Let's Get Started"
+            color='#9F02FF'
+            onPress={() => this.props.navigation.push('Profile')}
+          />
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    elevation: 10,
+    paddingHorizontal: 20,
+    paddingRight: 3,
+    paddingLeft: 3,
+    paddingTop: 10, //50
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 36,
+    fontFamily: 'AvenirNext-Regular',
+    textAlign: 'center',
+    //backgroundColor: 'blue',
+    marginTop: '20%',
+    marginBottom: '3%'
+  },
+  title: {
+    fontSize: 35,
+    fontFamily: 'AvenirNext-Regular',
+    color: '#9F02FF',
+    paddingHorizontal: '5%',
+    marginBottom: '1%',
+    textAlign: 'center',
+    height: 70,
+    //backgroundColor: 'red'
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0.5 },
+    shadowOpacity: 0.5,
+    shadowRadius: 0.4,
+  },
+  aboutText: {
+    fontSize: 16,
+    fontFamily: 'AvenirNext-Regular',
+    color: '#9F02FF',
+    paddingHorizontal: '3%',
+    marginBottom: '3%',
+    textAlign: 'center',
+    paddingRight: 5,
+    paddingTop: 5, //50
+    paddingLeft: 5,
+  },
+  background: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+  nextBut: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: '5%',
+  }
+});
