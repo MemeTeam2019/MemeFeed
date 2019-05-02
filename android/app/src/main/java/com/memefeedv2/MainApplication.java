d667c5b724585a9c17569b283184c0685c1aa3da
Siddhi Panchal <siddhipanchal@Siddhis-MBP.attlocal.net>
2019-05-02 14:33:18 -0700

    changes

--- android/app/src/main/java/com/memefeedv2/MainApplication.java
+++ android/app/src/main/java/com/memefeedv2/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +30,9 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+              new RNFirebaseAuthPackage(),
+              new RNFirebaseFirestorePackage()
       );
     }
 
