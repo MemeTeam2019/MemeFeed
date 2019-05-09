da922a0a984ad0b1f46e6d983d1b697aaf52b5c8
Mia Altieri <miaaltieri@eduroam-169-233-163-73.ucsc.edu>
2019-05-09 16:28:31 -0700

    basic stuff for atting

--- src/components/image/addComment.js
+++ src/components/image/addComment.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Button, TextInput, Keyboard } from 'react-native';
+import { FlatList, StyleSheet, View, Button, TextInput, Keyboard, Modal, Text, TouchableHighlight } from 'react-native';
 import firebase from 'react-native-firebase';
+import SearchResult from '../home/searchResult';
 
 class AddComment extends React.Component {
   constructor(props) {
@@ -8,9 +9,18 @@ class AddComment extends React.Component {
     this._onPressButton = this._onPressButton.bind(this);
     this.state = {
       height: 0,
-      text: ''
+      modalVisible: false,
+      text: '',
+      searchResults: [],
+      searchTerm: '',
+      mostRecentAt: -1
     };
   }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
 
   _onPressButton = () => {
     const user = firebase.auth().currentUser;
@@ -88,8 +98,115 @@ class AddComment extends React.Component {
     Keyboard.dismiss();
   };
 
+
+  /**
+   * Pulls all users whose username starts with the searchTerm
+   */
+  updateSearch = async (searchTerm = '') => {
+    console.log('updating search')
+    console.log(searchTerm)
+    // Set search term state immediately to update SearchBar contents
+    this.setState({ searchTerm });
+
+    const usersRef = firebase.firestore().collection('Users');
+    const lowerSearchTerm = searchTerm.toLowerCase();
+    let usernameMatches = [];
+    let nameMatches = [];
+
+    if (!searchTerm) {
+      this.setState({ searchResults: [] });
+      console.log('oof')
+      return;
+    }
+
+    usernameMatches = await usersRef
+      .where('searchableUsername', '>=', lowerSearchTerm)
+      .where('searchableUsername', '<', `${lowerSearchTerm}\uf8ff`)
+      .get()
+      .then((snapshot) => snapshot.docs)
+      .catch((err) => console.log(err));
+
+    nameMatches = await usersRef
+      .where('searchableName', '>=', lowerSearchTerm)
+      .where('searchableName', '<', `${lowerSearchTerm}\uf8ff`)
+      .get()
+      .then((snapshot) => snapshot.docs)
+      .catch((err) => console.log(err));
+
+    // Ensure there are no duplicates and your own profile doesn't show up
+    const combined = [...usernameMatches, ...nameMatches];
+    const searchResults = [];
+    const map = new Map();
+    const myUid = firebase.auth().currentUser.uid;
+    combined.forEach((snapshot) => {
+      if (!map.has(snapshot.ref.id) && myUid !== snapshot.ref.id) {
+        map.set(snapshot.ref.id);
+        searchResults.push(snapshot);
+      }
+    });
+    this.setState({ searchResults: searchResults.sort() });
+  };
+
+  renderSearchResult = (userRef) => {
+    const data = userRef.item.data();
+    const uid = userRef.item.ref.id;
+    return <SearchResult data={data} uid={uid} />;
+  };
+
   render() {
     return (
+      <View>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}>
+        <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TextInput
+              {...this.props}
+              multiline
+              autoCapitalize='none'
+              onChangeText={(text) => {
+                console.log(text)
+                if (text.length-1 < this.state.mostRecentAt){
+                  this.setModalVisible(!this.state.modalVisible);
+                }
+                this.state.searchTerm = text.substring(this.state.mostRecentAt+1, text.length);
+                this.updateSearch(this.state.searchTerm)
+                this.setState({ text })
+              }}
+              onContentSizeChange={(event) => {
+                this.setState({
+                  height: Math.min(120, event.nativeEvent.contentSize.height),
+                });
+              }}
+              style={[styles.input, { height: Math.max(35, this.state.height) }]}
+              value={this.state.text}
+            />
+
+            <FlatList
+              data={this.state.searchResults}
+              renderItem={(userRef) => this.renderSearchResult(userRef)}
+              keyExtractor={(item) => item.ref.id}
+            />
+
+
+            <TouchableHighlight
+              onPress={() => {
+                this.setModalVisible(!this.state.modalVisible);
+              }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+
       <View
         style={[
           styles.container,
@@ -101,7 +218,13 @@ class AddComment extends React.Component {
           multiline
           placeholder='Add a comment...'
           autoCapitalize='none'
-          onChangeText={(text) => this.setState({ text })}
+          onChangeText={(text) => {
+            if (text[text.length-1] === '@') {
+              this.setModalVisible(true);
+            }
+            this.state.searchTerm = 'f'
+            this.setState({ text, mostRecentAt: text.length-1})
+          }}
           onContentSizeChange={(event) => {
             this.setState({
               height: Math.min(120, event.nativeEvent.contentSize.height),
@@ -121,6 +244,7 @@ class AddComment extends React.Component {
           title='Post'
           color='#000'
         />
+      </View>
       </View>
     );
   }
---
+++ src/components/image/atResults.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import firebase from 'react-native-firebase';
+
+/**
+ * Component to display a search result when using the search feature.
+ * onPress will navigate to the profile of the user in question.
+ *
+ * Used by:
+ *    atResult.js
+ *
+ * Props:
+ *    data (Object): The data obtained from the DocumentSnapshot of some user,
+ *                   e.g. userSnapshot.data(). Refer to the Firebase Users
+ *                   collection for the expected object properties.
+ */
+class AtResult extends React.PureComponent {
+  render() {
+    const { navigation, data, uid } = this.props;
+    if (!data || !uid) return null;
+    return (
+      <React.Fragment>
+        <TouchableOpacity
+          onPress={() => navigation.push('FriendProfile', { uid })}
+          style={styles.resultContainer}
+        >
+          <View>
+            <Image style={styles.profilePic} source={{ uri: data.icon }} />
+          </View>
+          <View>
+            <Text style={styles.primaryText}>{data.username}</Text>
+            <Text style={styles.secondaryText}>{data.name}</Text>
+          </View>
+        </TouchableOpacity>
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  resultContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginHorizontal: '5%',
+    paddingVertical: '2.5%',
+  },
+  primaryText: {
+    color: '#000',
+    fontSize: 24,
+  },
+  secondaryText: {
+    color: '#6C757D',
+    fontSize: 18,
+  },
+  profilePic: {
+    height: 50,
+    width: 50,
+    marginRight: '5%',
+    borderRadius: 50 / 2,
+  },
+});
+
+export default AtResult;
