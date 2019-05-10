fde82e441899dcd8409110ebbad50ba33eab5079
jchong1 <jocchong@ucsc.edu>
2019-05-10 13:14:28 -0700

    uh

---
+++ src/recommender/data/meme.py
@@ -0,0 +1,14 @@
+class Meme:
+    def __init__(self, popularity):
+        self.popularity = 0
+        self.uid = ''
+
+    def vectorize(self):
+        return [list of fields]
+
+    def store_in_firebase(self):
+        # store data from the object
+
+    def update(self):
+        pass
+
