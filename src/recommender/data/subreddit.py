92eae1bcd4ba148ddc0d923a0db1cc03b69b53af
jchong1 <jocchong@ucsc.edu>
2019-05-14 15:49:00 -0700

    starting Pipeline class

--- src/recommender/data/subreddit.py
+++ src/recommender/data/subreddit.py
@@ -4,4 +4,13 @@ class Subreddit:
     def __init__(self, sub_id, data={}):
         try:
             self.sub_id = sub_id
+            self.last_reacted = data['lastReacted']
+            self.negative_weight = data['negativeWeight']
+            self.positive_weight = data['positiveWeight']
+        except Exception as e:
+            print(f'Subreddit instantiation failed for id: {self.sub_id}')
+            print(e)
+            raise e
 
+    def vectorize(self):
+        return [self.last_reacted, self.negative_weight, self.positive_weight]
