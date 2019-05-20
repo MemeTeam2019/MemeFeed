4f7edb91efc89e1dd2f801db2bbfc36a6464d595
jchong1 <jocchong@ucsc.edu>
2019-05-20 15:23:42 -0700

    changing rating to be rating + 1

--- src/recommender/itemBasedRecommendation.py
+++ src/recommender/itemBasedRecommendation.py
@@ -1,12 +1,15 @@
+from typing import list, dict
 from data import db
+
 firestore = db.db
+
 
 class ItemBasedRecommendation:
     def __init__(self):
         # Maps each meme_id to the uid of users who have reacted positively
-        self.uids = self.get_uids()
-        self.meme_ids = self.get_meme_ids()
-        self.rank_matrix = self.build_matrix()
+        self.uids: list = self.get_uids()
+        self.meme_ids: list = self.get_meme_ids()
+        self.rank_matrix: dict = self.build_matrix()
 
     # Fetch all uids from the Users collection
     def get_uids(self):
@@ -31,15 +34,24 @@ class ItemBasedRecommendation:
                 meme_id = meme.id
                 like_data = meme.to_dict()
                 if meme_id not in matrix:
-                    matrix[meme_id] = { uid: like_data['rank'] }
+                    matrix[meme_id] = {uid: like_data['rank'] + 1}
                 else:
-                    matrix[meme_id][uid] = like_data['rank']
+                    matrix[meme_id][uid] = like_data['rank'] + 1
         return matrix
 
     # All vectors need to have
-    def vectorize(self):
+    def complete_vector(self):
         for _, rank_dict in self.rank_matrix:
+            for meme_id in self.meme_ids:
+                if meme_id not in rank_dict:
+                    rank_dict[meme_id] = 0
 
+    # Return the list representation of ranks for the given item
+    def vectorize(self, meme_id: str) -> list:
+        vector = []
+        for _, rank in sorted(self.rank_matrix[meme_id]):
+            vector.append(rank)
+        return vector
 
     # Fetch all the reacts for the given uid
     def get_target_uid_reacts(self, uid):
@@ -56,10 +68,15 @@ class ItemBasedRecommendation:
             total += rank
         return total / len(rankings)
 
+    # Calculate the average rank a user has given
     def average_user_ranking(self, uid):
         pass
 
     # Calculate the similarity between memes, i and j
     def similarity(self, i, j):
         pass
+
+
+if __name__ == '__main__':
+    rec = ItemBasedRecommendation()
 
