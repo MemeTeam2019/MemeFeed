79f19d4cc20f83816bf9dd1fdd8f816d0db8f7b4
jchong1 <jocchong@ucsc.edu>
2019-05-26 15:02:04 -0700

    cleaning up more files

--- src/screens/signupPage.js
+++ src/screens/signupPage.js
@@ -52,7 +52,6 @@ class SignupScreen extends React.Component {
             followersLst: [],
             followingLst: [],
           });
-          return uid;
         }
       })
       .then(() => this.props.navigation.push('Icon'))
