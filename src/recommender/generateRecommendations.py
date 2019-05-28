59dea1a87c721594e0cc0133a931b894800553a3
Mia Altieri <mgaltier200@gmail.com>
2019-05-28 09:10:11 -0700

    basics for consolidation no firebase upload

--- src/recommender/generateRecommendations.py
+++ src/recommender/generateRecommendations.py
@@ -45,8 +45,88 @@ def updateSubredditVectors():
     pass
 
 # STEP 2 ----------------------------------------------------- consolidate votes
-def consolidateRecommendations(userVotes, itemVotes, subredditsVotes):
-    pass
+def consolidateRecommendations(userVotes, itemVotes, subredditVotes):
+    finalRecommendations = {}
+
+    uVotes = userVotes.keys()
+    mVotes = itemVotes.keys()
+    sVotes = subredditVotes.keys()
+    # umsVotes is the set that each algorithm found data on
+    umsVotes = (uVotes.intersect(mVotes)).intersect(sVotes)
+    # update their votes
+    uVotes = uVotes.difference(umsVotes)
+    mVotes = mVotes.difference(umsVotes)
+    sVotes = sVotes.difference(umsVotes)
+    # umVotes, usVotes, msVotes are the votes that two of the algorithms found
+    # data for
+    umVotes = uVotes.intersect(mVotes)
+    usVotes = uVotes.intersect(sVotes)
+    msVotes = mVotes.intersect(sVotes)
+    # uVotes, mVotes, sVotes now only contain the votes that one algorithm found
+    uVotes = (uVotes.difference(umVotes)).difference(usVotes)
+    mVotes = (mVotes.difference(umVotes)).difference(msVotes)
+    sVotes = (sVotes.difference(usVotes)).difference(msVotes)
+
+
+    # look at memes we have the most data for
+    for memeId in umsVotes:
+        uRank = userVotes[memeId]
+        mRank = itemVotes[memeId]
+        sRank = subredditVotes[memeId]
+        avgRank = (uRank + mRank + sRank)/3
+        if avgRank >= 2:
+            # confidence measures how many of the algorithms ranked this meme highly
+            confidence = findConfidence(uRank, mRank, sRank)
+            finalRecommendations[memeId] = {'predictedRank': avgRank,
+                                            'confidence': confidence }
+
+    finalRecommendations = voteUsingTwoGroups(umVotes, userVotes, itemVotes, finalRecommendations)
+    finalRecommendations = voteUsingTwoGroups(usVotes, userVotes, subredditVotes, finalRecommendations)
+    finalRecommendations = voteUsingTwoGroups(umVotes, itemVotes, subredditVotes, finalRecommendations)
+
+
+    # we are going to go thru all the suggested recommendations and put them in
+    # a hashmap
+
+    # the best ones we will give the highest time (to ensure they get shown first)
+    # best are the ones that all 3 decided the user would rank highly
+
+
+
+    return finalRecommendations
+
+# findConfidence returns the number of ranks that rated this positively
+def findConfidence(uRank, rRank, sRank):
+    confidence = 0
+    confidence = 1 if uRank > 1 else 0
+    confidence = confidence + 1 if sRank > 1 else confidence
+    confidence = confidence + 1 if mRank > 1 else confidence
+    return confidence
+
+# voteUsingTwoGroups predicts a recommendation when there are two groups that
+# have data for a meme
+def voteUsingTwoGroups(memeIds, aVotes, bVotes, recommendations):
+    for memeId in memeIds:
+        aRank = aVotes[memeId]
+        bRank = bVotes[memeId]
+        avgRank = (aRank + bRank)/2
+        if avgRank >= 2:
+            # confidence measures how many of the algorithms ranked this meme highly
+            confidence = findConfidence(aRank, bRank, 0)
+            recommendations[memeId] = {'predictedRank': avgRank,
+                                            'confidence': confidence }
+    return recommendations
+
+# voteUsingOneGroup predicts a recommendation when there is only one group that
+# has data for a meme
+def voteUsingOneGroup(memeIds, votes, bVotes, recommendation):
+    for memeId in memeIds:
+        rank = votes[memeId]
+        if rank >= 2:
+            recommendations[memeId] = {'predictedRank': avgRank,
+                                            'confidence': 1 }
+    return recommendations
+
 
 def main():
     # update vectors
