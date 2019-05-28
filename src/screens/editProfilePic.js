aa4062e34116008135b73ea9c51ea174d1cd7eed
Siddhi Panchal <siddhipanchal@eduroam-169-233-189-145.ucsc.edu>
2019-05-28 10:40:18 -0700

    profile pic changing

--- src/screens/editProfilePic.js
+++ src/screens/editProfilePic.js
@@ -11,6 +11,8 @@ import {
 } from 'react-native';
 
 import firebase from 'react-native-firebase';
+import ImagePicker from 'react-native-image-picker';
+import AutoHeightImage from 'react-native-auto-height-image';
 
 export default class EditProfilePic extends React.Component {
   static navigationOptions = {
@@ -25,24 +27,87 @@ export default class EditProfilePic extends React.Component {
 
   }
 
-  _onPressButton = async (newIcon) => {
-    const oldIcon = this.state.selectedIcon;
-    this.setState({
-      selectedIcon: newIcon === oldIcon ? false : newIcon,
-      icon: newIcon,
-    });
-    firebase
-      .firestore()
-      .collection('Users')
-      .doc(firebase.auth().currentUser.uid)
-      .update({
-        icon: this.iconImages[newIcon],
-      });
-  };
 
-  _onNextButton = async () => {
-    this.props.navigation.push('About');
-  };
+  componentDidMount() {
+    this._isMounted = true;
+    if (this._isMounted) {
+      const uid = firebase.auth().currentUser.uid;
+      // Get the profile icon
+      firebase
+        .firestore()
+        .collection('Users')
+        .doc(uid)
+        .get()
+        .then((docSnapshot) => {
+          if (docSnapshot.exists) {
+            const { icon } = docSnapshot.data();
+            this.setState({ icon });
+          }
+        })
+        .catch((error) => {
+          //console.log(error);
+        });
+      this.userListener = firebase
+        .firestore()
+        .collection('Users')
+        .doc(uid)
+        .get()
+        .then((snapshot) => this.setState(snapshot.data()));
+    }
+  }
+
+  chooseFile = () => {
+    var options = {
+      noData: true
+    };
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if(response.uri){
+        this.setState({ icon: response.uri,
+                        //filename: response.filename,
+                        //isChosen: true,
+
+        });
+        firebase
+          .firestore()
+          .collection('Users')
+          .doc(firebase.auth().currentUser.uid)
+          .update({
+            icon: response.uri,
+          });
+      }
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        let source = response;
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+        this.setState({
+          filePath: source,
+        });
+      }
+    });
+
+}
+
+
+ setPicture = () => {
+
+
+ }
+
+
+
+
+
+
 
   render() {
     return (
@@ -54,12 +119,19 @@ export default class EditProfilePic extends React.Component {
           <Text style={styles.title}> Edit Profile Picture </Text>
         </View>
         <View>
-          <Image source={require('../images/userIcons/icon888.png')}
-                 style={{ justifyContent: 'center', width: 180, height: 180, borderRadius: 180 / 2 }} />
+          <Image
+            source={{ uri: this.state.icon }}
+            style={{ width: 85, height: 85, borderRadius: 85 / 2 }}
+          />
         </View>
         <View>
-          <TouchableOpacity>
-          <Text style={styles.button}> Choose another profile picture </Text>
+          <TouchableOpacity onPress={this.chooseFile.bind(this)}>
+          <Text style={styles.button}> Change Profile Picture</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity onPress={this.setPicture.bind(this)}>
+          <Text style={styles.button}> Done </Text>
           </TouchableOpacity>
         </View>
       </ImageBackground>
