318ecec26f069b0132f0d933de7af89a4736b8dc
Mia Altieri <mgaltier200@gmail.com>
2019-05-28 19:17:37 -0700

    consolidation and voting on memes done

--- src/recommender/generateRecommendations.py
+++ src/recommender/generateRecommendations.py
@@ -16,7 +16,12 @@ def updateUserVectors():
     userReactions = {}
     userAverageReactions = {}
     users = db.collection('Users').stream()
+    i = 6
     for user in users:
+        i -= 1
+        if i == 0:
+            break
+
         u = User(user.id, user.to_dict())
         memes = {}
         for id, meme in u.get_reacts().items():
@@ -60,33 +65,31 @@ def consolidateRecommendations(userVotes, itemVotes, subredditVotes):
 
         # store each recommended meme in firebase
         for meme in finalRecommendations[user]:
-            rec_ref = db.reference(f'Recommendations/{user}/Memes')
-            doc_ref = db.collection('Memes').document(meme)
-            memeDoc = doc_ref.get()
-            memeData = memeDoc.to_dict())
-            rank = round(finalRecommendations[user]['predictedRank'])
-            modifiedTime = time + rank + finalRecommendations[user]['confidence']
+            rec_ref = db.collection(f'Recommendations/{user}/Memes').document(meme)
+            rank = round(finalRecommendations[user][meme]['predictedRank'])
+            modifiedTime = time.time() + rank + finalRecommendations[user][meme]['confidence']
 
-            # if the recommendation hasn't already been made and the predicted
-            # rank is high
-            if rank >= 2 and not db.reference(f'Recommendations/{user}/Memes/{meme}').exists:
+            recommendation_doc = rec_ref.get()
+            # if worth putting in the recommendations and its not already there
+            if rank >= 2 and recommendation_doc.to_dict() == None:
+                # if document doesn't exist, put it there
+                doc_ref = db.collection('Memes').document(meme)
+                memeDoc = doc_ref.get()
+                memeData = memeDoc.to_dict()
                 if memeData['sub'] != None :
-                    rec_ref.update({
+                    rec_ref.set({
                         'time': modifiedTime,
                         'url': memeData['url'],
                         'sub': memeData['sub']
                     })
 
                 else:
-                    rec_ref.update({
+                    rec_ref.set({
                         'time': modifiedTime,
                         'url': memeData['url'],
                         'author': memeData['author']
                     })
 
-
-    pprint.pprint(finalRecommendations)
-    # store recommendations on firebase
 
 
 # consolidateRecommendationsByUser creates the final set of recommendations for
@@ -94,20 +97,21 @@ def consolidateRecommendations(userVotes, itemVotes, subredditVotes):
 def consolidateRecommendationsByUser(user, userVotes, itemVotes, subredditVotes):
     finalRecommendations = {}
 
-    uVotes = userVotes.keys()
-    mVotes = itemVotes.keys()
-    sVotes = subredditVotes.keys()
+    uVotes = set(userVotes.keys())
+    mVotes = set(itemVotes.keys())
+    sVotes = set(subredditVotes.keys())
     # umsVotes is the set that each algorithm found data on
-    umsVotes = (uVotes.intersect(mVotes)).intersect(sVotes)
+    umsVotes = (uVotes.intersection(mVotes))
+    umsVotes = umsVotes.intersection(sVotes)
     # update their votes
     uVotes = uVotes.difference(umsVotes)
     mVotes = mVotes.difference(umsVotes)
     sVotes = sVotes.difference(umsVotes)
     # umVotes, usVotes, msVotes are the votes that two of the algorithms found
     # data for
-    umVotes = uVotes.intersect(mVotes)
-    usVotes = uVotes.intersect(sVotes)
-    msVotes = mVotes.intersect(sVotes)
+    umVotes = uVotes.intersection(mVotes)
+    usVotes = uVotes.intersection(sVotes)
+    msVotes = mVotes.intersection(sVotes)
     # uVotes, mVotes, sVotes now only contain the votes that one algorithm found
     uVotes = (uVotes.difference(umVotes)).difference(usVotes)
     mVotes = (mVotes.difference(umVotes)).difference(msVotes)
@@ -136,7 +140,7 @@ def consolidateRecommendationsByUser(user, userVotes, itemVotes, subredditVotes)
     return finalRecommendations
 
 # findConfidence returns the number of ranks that rated this positively
-def findConfidence(uRank, rRank, sRank):
+def findConfidence(uRank, mRank, sRank):
     confidence = 0
     confidence = 1 if uRank > 1 else 0
     confidence = confidence + 1 if sRank > 1 else confidence
@@ -159,7 +163,7 @@ def voteUsingTwoGroups(memeIds, aVotes, bVotes, recommendations):
 
 # voteUsingOneGroup predicts a recommendation when there is only one group that
 # has data for a meme
-def voteUsingOneGroup(memeIds, votes, bVotes, recommendation):
+def voteUsingOneGroup(memeIds, votes, recommendations):
     for memeId in memeIds:
         rank = votes[memeId]
         if rank >= 2:
@@ -176,12 +180,11 @@ def main():
 
     # get recommendations
     userVotes = generateRecommendationsByUser(userVectors, userReactions, userAverageReactions)
-    pprint.pprint(userVotes)
     # itemVotes = ''
     # subredditsVotes = ''
     #
-    # # vote on recommendations
-    # orderedRecommendations = consolidateRecommendations(userVotes, itemVotes, subredditsVotes)
+    # vote on recommendations
+    orderedRecommendations = consolidateRecommendations(userVotes, userVotes, userVotes)
 
     # return orderedRecommendations
 
