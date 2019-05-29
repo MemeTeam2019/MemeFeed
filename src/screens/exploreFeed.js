9b3575cba312a1c248309eedd2ba5ad8efd70f1c
Emma Pedersen <erpeders.ucsc.edu>
2019-05-29 16:39:35 -0700

    updated componentDidMount to limit number of memes to 7 and 8, instead of 15 for each collection

--- src/screens/exploreFeed.js
+++ src/screens/exploreFeed.js
@@ -55,7 +55,7 @@ class ExploreFeed extends React.Component {
         .firestore()
         .collection('Memes')
         .orderBy('time', 'desc')
-        .limit(15)
+        .limit(7)
         .get()
         .then((memesSnap) => {
           firebase
@@ -64,7 +64,7 @@ class ExploreFeed extends React.Component {
           .doc(firebase.auth().currentUser.uid)
           .collection('Memes')
           .orderBy('time', 'desc')
-          .limit(15)
+          .limit(8)
           .get()
           .then((recsSnap) => this.updateFeed(memesSnap, recsSnap))
         });
