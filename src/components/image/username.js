699d29229056464479717fb176fd3db677889272
jchong1 <jocchong@ucsc.edu>
2019-06-05 14:29:08 -0700

    adding makeCancelable back

--- src/components/image/username.js
+++ src/components/image/username.js
@@ -42,9 +42,26 @@ class Username extends React.Component {
   }
 
   componentWillUnmount() {
-    this.unsubscribe = null;
-    this._isMounted = false;
+    this.userPromise.cancel();
   }
+
+  makeCancelable = (promise) => {
+    let hasCanceled = false;
+
+    const wrappedPromise = new Promise((resolve, reject) => {
+      promise.then(
+        (value) =>
+          hasCanceled ? reject({ isCanceled: true, value }) : resolve(value),
+        (error) => reject({ isCanceled: hasCanceled, error })
+      );
+    });
+    return {
+      promise: wrappedPromise,
+      cancel: () => {
+        hasCanceled = true;
+      },
+    };
+  };
 
   goToUser() {
     // If going to our own profile
